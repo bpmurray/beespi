7ffe48aea6cbaad740b1d2d3f3aa51298dd2976f
Brendan Murray <brendanpmurray.at.gmail>
2015-04-21 19:44:06 -0400

    Updated scripts

--- setup.sh
+++ setup.sh
@@ -4,15 +4,20 @@
 PROJECTNAME=beespi
 PROJECTDIR=~/${PROJECTNAME}
 
+# Location of files
+GITFILES=`pwd`
+
 # Create a directory called beespi & change to it
 mkdir -p ${PROJECTDIR}
 cd ${PROJECTDIR}
 
+################## System ####################
 # Update the system software
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install build-essential subversion libjpeg8-dev imagemagick libv4l-0 libv4l-dev uvc dynctrl
+sudo apt-get install build-essential subversion libjpeg8-dev imagemagick libv4l-0 libv4l-dev uvc dynctrl git-core
 
+################## Node & Node-RED ####################
 # Get NODE
 wget http://node-arm.herokuapp.com/node_0.10.36_armhf.deb
 sudo dpkg -i node_0.10.36_armhf.deb
@@ -20,12 +25,15 @@ sudo dpkg -i node_0.10.36_armhf.deb
 # Install node-red
 sudo npm install -g --unsafe-perm  node-red
 
+################## WiringPi ####################
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+./build
+
+################## mjpg-streamer ####################
+cd ${PROJECTDIR}
 # Download the mjpg-streamer code
 svn co https://svn.code.sf.net/p/mjpg-streamer/code/mjpg-streamer/ mjpg-streamer
-
-
- node-red-contrib-dht-sensor
- node-red-contrib-ds18b20-sensor
 
 # Build mjpg-streamer
 cd mjpg-streamer
@@ -39,3 +47,37 @@ unzip mjpg-streamer-master.zip
 cd mjpg-streamer-master/mjpg-streamer-experimental
 make
 sudo make install
+
+################## System files ####################
+#  Set up system files
+sudo cat >>/boot/config.txt <<__EOF
+# Turn off the camera LED
+disable_camera_led=1
+
+# Set 1-wire GPIO pin number
+dtoverlay=w1-gpio,gpiopin=4
+device_tree=
+__EOF
+
+sudo cat >>/etc/modules <<__EOF
+snd-bcm2835
+w1-gpio
+w1-therm
+bcm2835_v4l2
+i2c-bcm2708
+i2c-dev
+__EOF
+
+################## Scripts ####################
+# Set up startup scripts
+sudo cp ${GITFILES}/node-redd /etc/init.d
+sudo chmod +x /etc/init.d/node-redd
+sudo update-rc.d node-redd defaults
+sudo cp ${GITFILES}/mjpg-streamerd /etc/init.d
+sudo chmod +x /etc/init.d/mjpg-streamerd
+sudo update-rc.d mjpg-streamerd defaults
+
+# Set up the node-red extras
+sudo ${GITFILES}/setuproot.sh
+
+
---
+++ setuproot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Setup the node-red files
+mkdir /root/.node-red
+cd /root/.node-red
+npm install node-red-contrib-dht-sensor node-red-contrib-ds18b20-sensor
+cd ..
+chown -R root:root .node-red
