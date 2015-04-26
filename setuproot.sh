9a53ff4b973c001f15eecc9b29c3f7b34acca821
Brendan Murray <brendanpmurray.at.gmail>
2015-04-25 21:09:37 -0400

    Allowed mkdir to run without messages

--- setuproot.sh
+++ setuproot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Setup the node-red files
-mkdir /root/.node-red
+mkdir -p /root/.node-red
 cd /root/.node-red
 npm install --unsafe-perm node-red-contrib-dht-sensor node-red-contrib-ds18b20-sensor
 cd ..
